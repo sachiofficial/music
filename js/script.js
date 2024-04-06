188d2bd018d4c66872d9c4eae1e5a43400f587b8
sachiofficial <148868639+sachiofficial@users.noreply.github.com>
2024-04-06 21:56:47 +0530

    hjfgdjtgjghkjg

--- js/script.js
+++ js/script.js
@@ -246,6 +246,3 @@ function filterFunction() {
 
 
 
-
-
-
--- js/search.js
+++ js/search.js
@@ -3,3 +3,93 @@
          $('html, body').scrollTop($("#play86").offset().top);
       });
    });
+
+   $(document).ready(function(){
+      $("#Alinde").click(function(){
+         $('html, body').scrollTop($("#play97").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#ApeHadhakam").click(function(){
+         $('html, body').scrollTop($("#play93").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#HadagannaMeHitha").click(function(){
+         $('html, body').scrollTop($("#play87").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#Hadakari").click(function(){
+         $('html, body').scrollTop($("#play96").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#Highwaywageyan").click(function(){
+         $('html, body').scrollTop($("#play89").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#KasiSabanPena").click(function(){
+         $('html, body').scrollTop($("#play94").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#MagaHaree").click(function(){
+         $('html, body').scrollTop($("#play88").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#MathakaMakala").click(function(){
+         $('html, body').scrollTop($("#play95").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#MithyaMayam").click(function(){
+         $('html, body').scrollTop($("#play85").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#Nimawa").click(function(){
+         $('html, body').scrollTop($("#play100").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#Nohithunata").click(function(){
+         $('html, body').scrollTop($("#play98").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#Zhurek").click(function(){
+         $('html, body').scrollTop($("#play92").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#NethManema").click(function(){
+         $('html, body').scrollTop($("#play91").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#HenaKetayam").click(function(){
+         $('html, body').scrollTop($("#play99").offset().top);
+      });
+   });
+
+   $(document).ready(function(){
+      $("#HeenaMiyadi").click(function(){
+         $('html, body').scrollTop($("#play90").offset().top);
+      });
+   });
